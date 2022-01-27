f39a7698a5e6db05ea8622801fa5c78609aef1bc
akhur <khurshedi_abdu@mail.ru>
2022-01-27 11:34:49 +0500

    actionMethod

--- src/actions/BaseAction.php
+++ src/actions/BaseAction.php
@@ -3,6 +3,7 @@
 namespace akhur\sberpay\actions;
 
 use yii\base\Action;
+use yii\base\Controller;
 use yii\base\InvalidConfigException;
 use yii\web\BadRequestHttpException;
 
@@ -12,7 +13,25 @@ use yii\web\BadRequestHttpException;
  */
 class BaseAction extends Action
 {
+    /**
+     * @var string the controller method that this inline action is associated with
+     */
+    public $actionMethod;
+
+
     public $callback;
+
+    /**
+     * @param string $id the ID of this action
+     * @param Controller $controller the controller that owns this action
+     * @param string $actionMethod the controller method that this inline action is associated with
+     * @param array $config name-value pairs that will be used to initialize the object properties
+     */
+    public function __construct($id, $controller, $actionMethod, $config = [])
+    {
+        $this->actionMethod = $actionMethod;
+        parent::__construct($id, $controller, $config);
+    }
 
     /**
      * @param mixed $orderID
